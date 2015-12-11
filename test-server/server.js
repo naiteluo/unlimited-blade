35e6a6599c586cb3be7f7d70fdb874d7785ee91a
tanjiawei <naiteluo@gmail.com>
2015-12-11 21:50:19 +0800

    add test server to create api service

--- test-server/server.js
+++ test-server/server.js
@@ -6,11 +6,31 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
+app.get('/user', function(req, res) {
+
+  var data = {
+    '1': {
+      name: 'abc',
+      age: 12
+    },
+    '2': {
+      name: 'test',
+      age: 233
+    }
+  };
+
+  var userId = req.query.id;
+  var userData = data[userId];
+
+  res.json(userData);
+});
+
 var server = app.listen(config.port, function() {
   var host = server.address().address;
   var port = server.address().port;
 
-  // console.log('Example app listening at http://%s:%s', host, port);
+  console.log('Example app listening at http://%s:%s', host, port);
 });
 
 exports.config = config;
+exports.app = app;
